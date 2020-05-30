39898948603790b9d6a65048065c61b89b89e8bf
Emily Littler <emilylittler1@gmail.com>
2020-05-30 14:50:30 +0100

    guardian api added, not working how i want

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
 const app = express();
-
 require('dotenv').config();
+
+
+const guardian = require('./lib/headline')
+
+let myArray = [];
+
+const tenHeadlines = async() => {
+    let data = await guardian();
+    for (let i = 0; i < 10 ; i++) {
+    myArray.push(data.response.results[i].webTitle);
+}
+    return myArray;
+}
+
+tenHeadlines()
+console.log(myArray);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,13 +28,25 @@ app.engine('.hbs', hbs({
     extname: 'hbs'
 }))
 
-app.set('view engine', '.hbs' );
+app.set('view engine', '.hbs');
 
-app.get('/', async (req, res) => {
-res.render('index');
+
+app.get('/', (req, res) => {
+    res.render('index');
+}) 
+
+
+app.get('/guardian', async (req, res) => {
+    let data = await guardian();
+    let headline = tenHeadlines()
+    // let headline2 = data.response.results[1].webTitle;
+    // let link = data.response.results[0].webUrl;
+    // let link2 = data.response.results[1].webUrl;
+    res.render('guardian', {headline});
 
 })
 
+
 app.listen(3000, () => {
-    console.log ('server is running');
+    console.log('server is running on port 3000');
 })
